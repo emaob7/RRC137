747aa11d5d210942f856a8e354e9de561694b975
emmaob7 <emanuelob7@gmail.com>
2023-10-01 12:55:43 -0300

    Initial Commit

---
+++ RRC137/RRC137App.swift
@@ -0,0 +1,17 @@
+//
+//  RRC137App.swift
+//  RRC137
+//
+//  Created by Emmanuel Ortigoza on 2023-10-01.
+//
+
+import SwiftUI
+
+@main
+struct RRC137App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RRC137UITests/RRC137UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  RRC137UITestsLaunchTests.swift
+//  RRC137UITests
+//
+//  Created by Emmanuel Ortigoza on 2023-10-01.
+//
+
+import XCTest
+
+final class RRC137UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
